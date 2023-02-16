cd19d992f1579116823fc9384666433f2ed22f3a
Rish <rish@users.noreply.github.com>
2023-02-16 23:48:47 +0000

    Initial commit

---
+++ app/assets/config/manifest.js
@@ -0,0 +1,6 @@
+//= link_tree ../images
+//= link_directory ../stylesheets .css
+//= link_tree ../../javascript .js
+//= link_tree ../../../vendor/javascript .js
+//= link fonts/font-awesome-4.3.0/css/font-awesome.min.css
+
---
+++ app/javascript/application.js
@@ -0,0 +1,11 @@
+// Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
+import "@hotwired/turbo-rails"
+import "controllers"
+import "/jquery.min"
+import "jquery.poptrox.min"
+import "jquery.scrolly.min"
+import "jquery.scollex.min"
+import "browser.min"
+import "breakpoints.min"
+import "util"
+import "main"
